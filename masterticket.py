0f9ccbc593bb0b7eb424ddc438494bcb03bac3db
Will <w255li@edu.uwaterloo.ca>
2019-07-02 18:22:52 -0400

    Finished code

--- masterticket.py
+++ masterticket.py
@@ -1,3 +1,23 @@
 TICKET_PRICE = 10
 
 tickets_remaining = 100
+
+#output how many ticket are remaining using the tickets_remaining variable
+
+print("There are {} tickets remaining".format(tickets_remaining))
+
+
+#gather the user name and assign it to a new variable
+
+user_name = input("What is your name?   ")
+
+#prompt the user by name and ask how many tickets the would like
+number_of_tickets = input("Hello {} how many tickets would you like".format(user_name))
+number_of_tickets = int(number_of_tickets)
+
+#caculate the price(number of tickets multiplied by the price) and assign it to a variable
+
+price = number_of_tickets* TICKET_PRICE
+
+# output the price to the screen
+print ("The total price is ${} ".format(price))
