f0eec5b9eab045622080b098799b1ae853cc6338
Will <w255li@edu.uwaterloo.ca>
2019-08-10 14:13:43 -0400

    Create characters.py

---
+++ object-oriented-python/characters.py
@@ -0,0 +1,22 @@
+#Remember first letter capital for classes!!
+#understanding self. Self has to be used because a method belongs to a class but it is being called on an instance
+import random
+
+
+class Thief:
+	sneaky = True
+
+	def pickpocket(self):
+		print("Called by {}".format(self))
+		if (self.sneaky == True):
+			return bool(random.randint(0,1))
+		else:
+			return False
+
+kenneth = Thief()
+#the self tells the pickpocket method to refer to kenneth
+print(kenneth.pickpocket())
+#same as 
+print(Thief.pickpocket(kenneth))
+
+kenneth
